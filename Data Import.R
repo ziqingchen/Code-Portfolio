0d3f60841e0a155a5840f1634079ca696b2f206a
ziqingchen <52891822+ziqingchen@users.noreply.github.com>
2019-07-21 13:27:22 -0400

    Update Data Import.R

--- Data Import.R
+++ Data Import.R
@@ -71,4 +71,136 @@ fromweb <- read.table(file = 'http://goo.gl/jTNf6P',
                       header = TRUE)
 # I read a text file from the web
 
+setwd(dir = "/Users/tsing/Desktop/RWorkflow/Code-Portfolio/week3")
+# I changed my working directory to "/Users/tsing/Desktop/RWorkflow/Code-Portfolio/week3"
+
+library(readr)
+ozone <- read_csv("data/US EPA data 2017.csv")
+# I loaded the .csv file
+
+names(ozone) <- make.names(names(ozone))
+# I rewrote the name of the columns to remove any spaces.
+
+nrow(ozone)
+# the number of rows is 66869
+
+ncol(ozone)
+# the number of rows is 55
+
+str(ozone)
+# the classes of each of the columns are correctly specified
+
+head(ozone[, c(6:7, 10)])
+tail(ozone[,c(6:7, 10)])
+# I peek at the top and bottom of the data
+
+table(ozone$Sample.Duration)
+# I looked at the Sample Duration variable to see what durations are recorded as being taken.
+
+library(dplyr)
+filter(ozone, Sample.Duration == "15 MINUTE") %>% 
+select(State.Name, County.Name, 
+       Sample.Duration, Arithmetic.Mean)
+# I took a look at which observations were measured at 15-minute duration
+
+filter(ozone, State.Code == "11"
+       County.Code == "954"
+       Sample.Duration == "15 MINUTE") %>%
+  select(Arithmetic.Mean)%>%
+  as.data.frame
+
+x <- 1:5
+y <- 6:10
+z <- 11:15
+# I created 3 vectors of length 5
+
+cbind(x, y, z)
+# I created a matrix where x, y and z are columns
+
+rbind(x, y, z)
+# I created a matrix where x, y and z are rows
+
+cbind(c(1, 2, 3, 4, 5),
+      c("a", "b", "c", "d", "e"))
+# I created a matrix with numeric and character columns
+
+matrix(data = 1:20,
+       nrow = 5,
+       ncol = 4)
+# I created a matrix containing the values from 1 to 20 with 5 rows and 4 columns
+
+matrix(data = 1:20,
+       nrow = 4,
+       ncol = 5)
+# I created a matrix containing the values from 1 to 20 with 4 rows and 5 columns
+
+matrix(data = 1:20,
+       nrow = 4,
+       ncol = 5,
+       byrow = TRUE)
+# now with 4 rows and 5 columns, but fill by row instead of columns
+
+survey <- data.frame("index" = c(1, 2, 3, 4, 5),
+                     "sex" = c("m", "m", "f", "f", "f"),
+                     "age" = c(34, 89, 26, 53, 33))
+str(survey)
+# I showed the structure of the survey
+## 'data.frame':	5 obs. of  3 variables:
+### $ index: num  1 2 3 4 5
+#### $ sex  : Factor w/ 2 levels "f","m": 2 2 1 1 1
+##### $ age  : num  34 89 26 53 33
+
+survey <- data.frame("index" = c(1, 2, 3, 4, 5),
+                     "sex" = c("m", "m", "m", "f", "f"),
+                     "age" = c(99, 46, 23, 54, 23),
+                     stringsAsFactors = FALSE)
+str(survey)
+# no more factors
+## 'data.frame':	5 obs. of  3 variables:
+### $ index: num  1 2 3 4 5
+#### $ sex  : chr  "m" "m" "m" "f" ...
+##### $ age  : num  99 46 23 54 23
+
+
+pirates <- data.frame("name" = c("Astrid", "Lea", "Sarina", "Remon", "Letizia", "Babice", "Jonas", "Wendy", "Niveditha", "Gioia"),
+                     "sex" = c("f", "f", "f", "m", "f", "f", "m", "f", "f", "f"),
+                     "age" = c(30, 25, 25, 29, 22, 22, 35, 19, 32, 21),
+                     "Superhero" = c("Batman", "Superman", "Batman", "Spiderman", "Batman", "Antman", "Batman", "Superman", "Maggott", "Superman"),
+                     "Tattoos" = c(11, 15, 12, 5, 65, 3, 9, 13, 900, 0),
+                     stringsAsFactors = FALSE)
+str(pirates)
+# typed in objects for pirates
+
+median(pirates$age)
+# the median age of 10 pirates is 25
+
+aggregate(formula = age ~ sex,
+          data = pirates,
+          FUN = mean)
+# female mean age: 24.5; male mean age: 32
+
+with(pirates, 
+     max(Tattoos[sex == "m"]))
+# 9 was the most number of tattoos owned by a male pirate
+
+with(pirates, 
+     mean(sex[age < 32] == "f"))
+# 87.5% of pirates under the age of 32 were female
+
+with(pirates, 
+     mean(age[sex == "f"] < 32))
+# 87.5% of female pirates were under the age of 32
+
+pirates$tattoos.per.year <- with(pirates, Tattoos / age)
+
+pirates$name[pirates$tattoos.per.year == max(pirates$tattoos.per.year)]
+# Niveditha had the most number of tattoos per year
+
+pirates$name[with(pirates, sex == "f" & Superhero == "Superman")]
+# Lea, Wendy, and Gioia are the female pirates whose favorite piratesurvey is Superman
+
+with(pirates, (Tattoos[age > 20 & Superhero == "Spiderman"]))
+# 5 is the median number of tattoos of pirates over the age of 20 whose favorite superhero is Spiderman
+
+
 
